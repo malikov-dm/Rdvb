5ad9534bd2a9b39bbaec2e5af85fa3bbdb9800d3
dmalikov <dmalikov@rdvb.ru>
2018-12-06 14:48:27 +0300

    example for russian post api connection

---
+++ Rdvb/Rdvb.RP.Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rdvb.RP.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+                RpCall.GetSoapMessageAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"There was an exception: {ex.ToString()}");
+            }
+        }
+    }
+}
---
+++ Rdvb/Rdvb.RP.Lab/RpCall.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdvb.RP.Lab
+{
+    class RpCall
+    {
+        public static async Task GetSoapMessageAsync()
+        {
+            var operationHistoryRequest = new OperationHistoryRequest() { Barcode = "", MessageType = 0, Language = "RUS" };
+            var authorizationHeader = new AuthorizationHeader() { login = "", password = "" };
+            authorizationHeader.mustUnderstand = "1";
+
+            var client = new OperationHistory12Client();
+
+            var resp = await client.getOperationHistoryAsync(operationHistoryRequest, authorizationHeader);
+
+            var res = JsonConvert.SerializeObject(resp.OperationHistoryData, Formatting.Indented);
+            Console.WriteLine(res);
+        }
+    }
+}
